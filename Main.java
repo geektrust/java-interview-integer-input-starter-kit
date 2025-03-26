e198e39348b6d39614916e60e0e22cb3c74106f1
Dhanush Gopinath <dhanush@geektrust.in>
2025-03-26 11:32:27 +0530

    feat: wrapped up the solution around Main.java and hiding other items from theia

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main{
 
     public static void main(String[] args){
         int n = Integer.parseInt(args[0]);
-
+        String output = new Solution().handle(n);
+        System.out.println(output);
     }
 }
---
+++ Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    /**
+     * Processes the given number and returns a string after implementing the logic for the problem
+     *
+     * @param number the input number to be processed
+     * @return a string or {@code null} if
+     *         no valid result is determined
+     */
+    public String handle(int number) {
+        return null;
+    }
+}
